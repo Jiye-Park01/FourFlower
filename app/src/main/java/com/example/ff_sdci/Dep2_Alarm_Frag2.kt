1e6fd0e001dc8f82c2e6e88f2cb3c50d88aef32c
amazing0827 <amazing0827@naver.com>
2023-11-25 05:01:24 +0900

    alarm xml

---
+++ app/src/main/java/com/example/ff_sdci/Dep2_Alarm_Frag2.kt
@@ -0,0 +1,18 @@
+package com.example.ff_sdci
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Dep2_Alarm_Frag2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dep2__alarm__frag1, container, false)
+    }
+}
