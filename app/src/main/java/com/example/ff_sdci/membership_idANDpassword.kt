9e2e4f2b55db63caa6f52792175cc89a5f04daf4
Jiye-Park01 <jiye0710@naver.com>
2023-11-25 03:54:17 +0900

    xml 추가

---
+++ app/src/main/java/com/example/ff_sdci/membership_idANDpassword.kt
@@ -0,0 +1,11 @@
+package com.example.ff_sdci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class membership_idANDpassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_membership_id_andpassword)
+    }
+}
